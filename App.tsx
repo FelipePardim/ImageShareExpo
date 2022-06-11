6fab02adaeda6090917e0a2962fe8dbf1918401e
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-06-11 12:28:24 -0300

    Adding logo to app screen and pich photo button

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#888', fontSize: 18}}> 
+       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+
+      <Text style={styles.instructions}> 
         To share a photo from your phone with a friend, just press the button below!</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +25,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
